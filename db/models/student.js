10a2a065a30609a39b1f1377565bab4e52db7fee
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-08-15 10:07:11 -0500

    passing cycle 1 specs

--- db/models/student.js
+++ db/models/student.js
@@ -2,7 +2,30 @@
 
 const Sequelize = require('sequelize');
 const db = require('../db');
+const sequelize = require('sequelize');
 
-const Student = db.define();
+const Student = db.define('student', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        }
+    },
+});
+
+Student.beforeCreate((student) => {
+    student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
+    student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1)
+
+})
 
 module.exports = Student;
